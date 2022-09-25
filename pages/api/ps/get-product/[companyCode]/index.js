405f7c6dbf3b90db5fb5cfac69a3045afa6cfbe8
Manish Chiniwalar <hi@manishrc.com>
2022-09-25 13:43:34 +0200

    feat: add getProductSellable endpoint

---
+++ pages/api/ps/get-product/[companyCode]/index.js
@@ -0,0 +1,36 @@
+import { PromoStandards } from "promostandards-sdk-js";
+
+export default async function handler(req, res) {
+  const { companyCode, productId } = req.query;
+
+  // const supplier = await prisma.supplier.findUnique({
+  //   where: {
+  //     id: supplierId,
+  //   },
+  // });
+
+  // const psClient = new PromoStandards.Client(supplier.psConfig);
+  const psClient = new PromoStandards.Client({
+    name: "PCNA",
+    id: process.env.PCNA_PS_ID,
+    password: process.env.PCNA_PS_PASSWORD,
+    endpoints: [
+      {
+        type: "ProductData",
+        version: "2.0.0",
+        url: "https://psproductdata200.pcna.online/",
+      },
+    ],
+  });
+
+  const psResponse = await psClient.productData.getProductSellable({
+    isSellable: true,
+  });
+
+  res.setHeader(
+    "Cache-Control",
+    "public, s-maxage=600, stale-while-revalidate=3600"
+  );
+
+  res.status(200).json(psResponse);
+}
