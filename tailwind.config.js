6ac4cdbee3e1894f406d790c5e96165de183d61b
Manish Chiniwalar <hi@manishrc.com>
2022-09-25 13:36:39 +0200

    wip: example product page

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,9 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+  ],
 };
