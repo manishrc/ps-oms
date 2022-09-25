e34fca58472dcbcb369b65029976bbd78049ec93
Manish Chiniwalar <hi@manishrc.com>
2022-09-25 17:59:19 +0200

    wip: add shipping rate api

---
+++ pages/api/shipping/rates.js
@@ -0,0 +1,27 @@
+const carrier_ids = ["se-3115691", "se-3115692"];
+
+export default async function handler(req, res) {
+  const { ship_from, ship_to, packages } = req.body;
+
+  const response = await fetch("https://api.shipengine.com/v1/rates", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "API-Key": process.env.SHIPENGINE_API_KEY,
+    },
+    body: JSON.stringify({
+      rate_options: {
+        carrier_ids,
+      },
+      shipment: {
+        ship_from,
+        ship_to,
+      },
+      packages,
+    }),
+  });
+
+  console.log(await response.json());
+
+  res.status(200).json({ ship_from, ship_to, packages });
+}
